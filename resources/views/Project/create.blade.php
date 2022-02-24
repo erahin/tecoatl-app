d98489f06c9721f0f27fead64ef511b74ec0e159
17920368 <avictor.zrodriguez@gmail.com>
2022-02-24 17:34:27 -0600

    tecoatl-app

--- resources/views/Project/create.blade.php
+++ resources/views/Project/create.blade.php
@@ -27,7 +27,7 @@
                             <div class="row mb-3">
                                 {!! Form::label('region_id', 'Regíon', ['class' => 'col-md-4 col-form-label text-md-end']) !!}
                                 <div class="col-md-6">
-                                    {!! Form::select('region_id', $regions, $project->region_id, ['class' => 'form-select', 'id' => 'region_id']) !!}
+                                    {!! Form::select('region_id', $regions, $project->region_id, ['class' => 'form-select', 'id' => 'region_id', 'placeholder' => 'Seleccione Región']) !!}
                                 </div>
                             </div>
                             <div class="row mb-3">
