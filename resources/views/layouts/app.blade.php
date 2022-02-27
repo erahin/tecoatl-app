b9709023eaef9f759025675b19b33a7798de98f9
17920368 <avictor.zrodriguez@gmail.com>
2022-02-27 11:23:29 -0600

    tecoatl-app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-
+    <script src="{{ asset('js/script.js') }}" defer></script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -42,108 +42,116 @@
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
+                    <ul class="navbar-nav ms-auto grt-menu">
                         <!-- Authentication Links -->
                         @guest
-                        @if (Route::has('login'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @endif
+                            @if (Route::has('login'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
 
-                        @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                        @endif
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
                         @else
-                        {{-- <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('OPCIÓN') }}</a>
-                        </li> --}}
-                        <li class="nav-item dropdown text-uppercase">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ 'Proyectos' }}
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('estudios.index') }}">
-                                    {{ __('Estudios') }}
+                            {{-- <li class="nav-item">
+                                <a class="nav-link gtr-menu__li" href="{{ url('/') }}">{{ __('INICIO') }}</a>
+                            </li> --}}
+                            <li class="nav-item dropdown text-uppercase">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ 'Proyectos' }}
                                 </a>
-                                <hr class="dropdown-divider">
-                                <a class="dropdown-item" href="{{ route('regiones.index') }}">
-                                    {{ __('Regiones') }}
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('estudios.index') }}">
+                                        {{ __('Estudios') }}
+                                    </a>
+                                    <hr class="dropdown-divider">
+                                    <a class="dropdown-item" href="{{ route('regiones.index') }}">
+                                        {{ __('Regiones') }}
+                                    </a>
+                                    <hr class="dropdown-divider">
+                                    <a class="dropdown-item" href="{{ route('proyectos.index') }}">
+                                        {{ __('Proyectos') }}
+                                    </a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown text-uppercase">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ 'OPCIÓN' }}
                                 </a>
-                                <hr class="dropdown-divider">
-                                <a class="dropdown-item" href="{{ route('proyectos.index') }}">
-                                    {{ __('Proyectos') }}
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();">
+                                        {{ __('A') }}
+                                    </a>
+                                    <hr class="dropdown-divider">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();">
+                                        {{ __('b') }}
+                                    </a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown text-uppercase">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ 'OPCIÓN' }}
                                 </a>
-                            </div>
-                        </li>
-                        <li class="nav-item dropdown text-uppercase">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ 'OPCIÓN' }}
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();">
-                                    {{ __('A') }}
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();">
+                                        {{ __('A') }}
+                                    </a>
+                                    <hr class="dropdown-divider">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();">
+                                        {{ __('b') }}
+                                    </a>
+                                </div>
+                            </li>
+                            <li class="nav-item dropdown text-uppercase">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ 'OPCIÓN' }}
                                 </a>
-                                <hr class="dropdown-divider">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();">
-                                    {{ __('b') }}
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();">
+                                        {{ __('A') }}
+                                    </a>
+                                    <hr class="dropdown-divider">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();">
+                                        {{ __('b') }}
+                                    </a>
+                                </div>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link"
+                                    href="{{ route('usuarios.index') }}">{{ __('USUARIOS') }}</a>
+                            </li>
+                            <li class="nav-item dropdown text-uppercase">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
                                 </a>
-                            </div>
-                        </li>
-                        <li class="nav-item dropdown text-uppercase">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ 'OPCIÓN' }}
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();">
-                                    {{ __('A') }}
-                                </a>
-                                <hr class="dropdown-divider">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();">
-                                    {{ __('b') }}
-                                </a>
-                            </div>
-                        </li>
-                        <li class="nav-item dropdown text-uppercase">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ 'OPCIÓN' }}
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();">
-                                    {{ __('A') }}
-                                </a>
-                                <hr class="dropdown-divider">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();">
-                                    {{ __('b') }}
-                                </a>
-                            </div>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('usuarios.index') }}">{{ __('USUARIOS') }}</a>
-                        </li>
-                        <li class="nav-item dropdown text-uppercase">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();
-                                                                                     document.getElementById('logout-form').submit();">
-                                    {{ __('SALIR') }}
-                                </a>
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                                                                                                                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('SALIR') }}
+                                    </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                        class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
                         @endguest
                     </ul>
                 </div>
